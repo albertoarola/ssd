909ae128ac1b69e1aec558b2db8f8b45f54dc249
albertoarola <albertoarola@gmail.com>
2024-05-23 16:06:39 +0200

    gestione sessione tra microservizi e modifica logout

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify, make_response
 from flask_ldap3_login import LDAP3LoginManager, AuthenticationResponseStatus
 from flask_ldap3_login.forms import LDAPLoginForm
 from ldap3 import Server, Connection, ALL, HASHED_SALTED_SHA256, ALL_ATTRIBUTES
 from ldap3.core.exceptions import LDAPException
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, verify_jwt_in_request
+from flask_jwt_extended.exceptions import NoAuthorizationError
+import uuid
+
 
 app = Flask(__name__)
 
 app.secret_key = 'your_secret_key'
+
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+jwt = JWTManager(app)
 
 LDAP_HOST = 'ldap://ldap-server'
 LDAP_BASE_DN = 'dc=mycompany,dc=com'
@@ -77,7 +84,10 @@ def register():
         password = request.form['password']
     
         if add_user_to_ldap(username, firstname, lastname, email, password):
-            return redirect('http://localhost/home')
+            session['username'] = username
+            response = redirect('http://localhost/home')
+            response.set_cookie('username', username)
+            return response
         else:
             return "Aggiunta dell'utente fallita"
 
@@ -91,16 +101,20 @@ def login():
         password = request.form['password']
         if authenticate_ldap(username, password):
             session['username'] = username
-            return redirect('http://localhost/home')
+            response = redirect('http://localhost/home')
+            response.set_cookie('username', username)
+            return response
         else:
             return 'Autenticazione fallita'
     return render_template('login.html')
 
 # Logout
-@app.route('/logout')
-def logout():
+@app.route('/invalidate')
+def invalidate():
     session.pop('username', None)
-    return redirect(url_for('login'))
+    response = redirect(url_for('login'))
+    response.delete_cookie('username')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
